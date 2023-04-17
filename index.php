2bff04943a36fbea72133f47f343ad2ddd743be8
BallaDeepika <deepikaballa01@gmail.com>
2023-04-17 10:59:32 +0530

    two

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
           if($user_data['password']===$password)
           {
             $_SESSION['user_id']=$user_data['user_id'];
-            echo "hello";
+            echo "<script> location.href='start.html'; </script>";
            // header("Location:start.html");
             die;
           }
